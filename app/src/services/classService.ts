53253a807b7f6c6f2f5da1d52252825c51583c13
Brent Becker <wirrem1990@gmail.com>
2026-06-12 21:04:28 +0200

    cloudflare change

--- app/src/services/classService.ts
+++ app/src/services/classService.ts
@@ -1,6 +1,6 @@
 import type { ClassDto } from '../data/types';
 
-const API_BASE_URL = 'https://studentportaltest-a9afaje8c3hjctap.southafricanorth-01.azurewebsites.net/api/classes';
+const API_BASE_URL = 'https://proud-mouse-865e.wirrem1990.workers.dev/api/classes';
 
 export async function getClasses(): Promise<ClassDto[]> {
   const response = await fetch(`${API_BASE_URL}`);
